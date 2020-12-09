65dceff20628122d438cbfba3839d484c22084a1
只会番茄炒蛋 <673395239@qq.com>
2020-12-09 16:56:54 +0800

    deleteOriginalAssets  false

--- vue.config.js
+++ vue.config.js
@@ -80,7 +80,7 @@ module.exports = {
         test: /\.js$|\.html$|\.json$|\.css$|\.ttf$/,
         threshold: 10240, // 只有大小大于该值的资源会被处理
         minRatio: 0.8, // 只有压缩率小于这个值的资源才会被处理
-        deleteOriginalAssets: true // 删除源文件
+        deleteOriginalAssets: false // 删除源文件
       }])
       .end()
 
